ed333ce1d9c02ebb453b04cde1e93327e47d6944
Ryan Lee <ryalee@cisco.com>
2016-01-19 16:55:11 -0800

    01-print

---
+++ 01-print.py
@@ -0,0 +1,23 @@
+#using print format
+name = 'ryalee'
+age = 37
+print '{0} is man with {1}'.format(name, age)
+
+# using escape
+print 'a',
+print 'b',
+
+print 'c'
+print 'd'
+
+# using raw_input() and print parameters
+print "input your age"
+age = raw_input()
+print "input your name"; name = raw_input()
+print "00 your age is %s \nyour name is %s" %(age,name)
+print "01 your age is {0} \
+your name is {1}".format(age,name)
+
+
+
+
